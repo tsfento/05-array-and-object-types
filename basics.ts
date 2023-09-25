9e77a7f405b0e4dfcc6a60b8c98c74ddf168b08f
Tyler Fenton <tsfento@gmail.com>
2023-09-24 19:32:37 -0500

    init commit

---
+++ basics.ts
@@ -0,0 +1,20 @@
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+let person: {
+    name: string;
+    age: number;
+};
+
+person = {
+    name: 'Max',
+    age: 32,
+};
+
+let people: {
+    name: string;
+    age: number;
+}[];
